cc7a17d2872ffe07f2c8316afaa9a31a0ba8c0ea
Toni Moringa <your-moriil@example.com>
2026-04-21 04:58:17 +0300

    second commit

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
+// Step 1: Require datejs for date formatting
+require('datejs');
 
+// Step 1: Create combineUsers function with rest parameter ...args
+function combineUsers(...args) {
+  // Step 2: Initialize the Return Object
+  const combinedObject = {
+    users: [],
+  };
 
+  // Step 3: Loop through args to isolate each array
+  for (let i = 0; i < args.length; i++) {
+    // Step 4: Merge Arrays using spread operator
+    combinedObject.users = [...combinedObject.users, ...args[i]];
+  }
 
+  // Step 5: Get today's date with datejs in M/d/yyyy format
+  combinedObject.merge_date = Date.today().toString('M/d/yyyy');
+
+  // Step 7: Return object
+  return combinedObject;
+}
+
+// Export the function
 module.exports = {
-  ...(typeof combineUsers !== 'undefined' && { combineUsers })
+  ...(typeof combineUsers !== 'undefined' && { combineUsers }),
 };
